976012e16f7e8d162dc889199f9654d1ad6c23c2
unknown <felipe12091@hotmail.com>
2026-08-20 20:06:55 -0500

    Refactorizado refactor_pep8.py

--- ejercicios-clase/semana-02/refactor_pep8.py
+++ ejercicios-clase/semana-02/refactor_pep8.py
@@ -1,8 +1,22 @@
-def CalcularPromedio(Lista):
-    s=0
-    for x in Lista:
-     s=s+x
-    return s/len(Lista)
+def calcular_promedio(lista_numeros: list) -> float:
+    """Calcula el promedio de una lista de números.
  
-l=[1,2,3,4,5]
-print(CalcularPromedio(l))
+    Args:
+        lista_numeros: lista de números a evaluar.
+ 
+    Returns:
+        float: promedio aritmético de los números en la lista.
+    """
+    sumatoria = 0
+    for x in lista_numeros:
+        sumatoria = sumatoria + x
+    return sumatoria / len(lista_numeros)
+
+def main() -> None:
+    """Función principal del programa."""
+    lista = [1, 2, 3, 4, 5]
+    promedio = calcular_promedio(lista)
+    print(promedio)
+
+if __name__ == "__main__":
+    main()
